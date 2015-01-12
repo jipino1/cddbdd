44c7462f263e3c1be5f6133834518f132f212a76
Jimmy Pino <jimmypino@Jimmys-MacBook-Pro.local>
2015-01-11 21:52:13 -0500

    new sources

--- javascripts/app/view/PanelView.js
+++ javascripts/app/view/PanelView.js
@@ -15,8 +15,13 @@ define(['underscore', 'backbone', 'swig', 'helpers/events', 'plugins/text!templa
 
 			_.bindAll(this);
 			
-			view.initiate;
-
+			view.back;
+			view.next;
+			view.currentPanel = "panel-1";
+			view.panelContent;
+			view.panelCounter;
+			view.sectionCounter = 1;
+			
 			view.render();
 
 			console.log('Backbone : Global : PanelView : Initialized');
@@ -28,12 +33,44 @@ define(['underscore', 'backbone', 'swig', 'helpers/events', 'plugins/text!templa
 			view.panelTemplate = swig.compile(PanelTemplate);
 			view.$el.append(view.panelTemplate());
 			
+			view.panelContent = view.$el.find("#panel-content");
+			view.back = view.$el.find("#back-btn");
+			view.next = view.$el.find("#next-btn");
+			view.panelCounter = view.$el.find(".panel-counter .counter");
+
 			view.initPanelInteraction();
 		},
 		
 		"initPanelInteraction": function () {
 			var view = this;
 			
+			view.back.on("click", function (e) {
+				e.preventDefault();
+				
+				var bgPos = -18 * view.sectionCounter;
+				
+				if (view.sectionCounter > 1) {
+					view.panelContent.find("#panel-" + view.sectionCounter).fadeOut("slow", function () {
+						view.panelContent.find("#panel-" + (view.sectionCounter-1)).fadeIn();
+						view.sectionCounter--;
+					});
+				}
+			});
+			
+			view.next.on("click", function (e) {
+				e.preventDefault();
+				
+				var bgPos = -18 * view.panelCounter;
+
+				if (view.sectionCounter < 9) {
+					view.panelContent.find("#panel-" + view.sectionCounter).fadeOut("slow", function () {
+						view.panelContent.find("#panel-" + (view.sectionCounter+1)).fadeIn();
+						view.panelCounter.css({"background-position" : "0 " + bgPos + "px"})
+						
+						view.sectionCounter++;
+					});
+				}
+			});
 		}
 
 	});
